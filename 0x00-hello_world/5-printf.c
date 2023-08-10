ba23da7a0792111b3bfd16b1bc4ff4ae59726e17
Theophilousjoy <theophilousjoy@gmail.com>
2023-08-10 08:18:18 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints exact line of code and followed by a new line
+ *
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
