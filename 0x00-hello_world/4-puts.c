c59a4f84d06a007cb1eea1e83f2ff1a25d564ad3
Theophilousjoy <theophilousjoy@gmail.com>
2023-08-10 08:11:02 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line of action with puts function
+ *
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
