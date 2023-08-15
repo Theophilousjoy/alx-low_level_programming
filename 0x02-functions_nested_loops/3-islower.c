01b6c49c9a5de7dcf7f02afa69a89f605d196056
Theophilousjoy <theophilousjoy@gmail.com>
2023-08-15 09:18:56 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - A function that checks for lowercase character
  * @c : character to be verified
- * Return : 0 (True)
+ * Return : 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
