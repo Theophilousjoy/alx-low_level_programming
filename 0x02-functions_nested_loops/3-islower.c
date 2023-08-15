b1587e5c29a34e8facb0b53203a3c0bca85fd62c
Theophilousjoy <theophilousjoy@gmail.com>
2023-08-15 07:01:21 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,9 @@
  *
  * Return : 0 or 1 (True or False)
  */
-int _islower(int alph)
+int _islower(int c)
 {
-	if (alph >= 'a' && alph <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 
 	return (0);
