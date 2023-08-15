d707196dcbd4f383e4f6b80788e35fe7c10bde0a
Theophilousjoy <theophilousjoy@gmail.com>
2023-08-15 06:37:28 -0700

    Print 10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,13 @@
  * Return: 0 (True)
  */
 void print_alphabet_x10 (void)
-
 {
 	int num = 0;
 	int alph;
-	while(num++ <= 9)
+	while (num++ <= 9)
 	{for (alph = 'a'; alph <= 'z'; alph++)
 		_putchar(alph);
 
-			_putchar(10);
+		_putchar(10);
 	}
 }
