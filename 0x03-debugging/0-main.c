9bfeea05d98658508b35159d7f576d330f52f79c
Theophilousjoy <theophilousjoy@gmail.com>
2023-08-16 03:01:50 -0700

    Like, comment, subscribe

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
   * main - Function that test for positive or negative
   *
   * Return: 0 (Success)
   */
-
 int main(void)
 {
 	int i;
-
 	i = 0;
+
 	positive_or_negative(i);
 	
 	return (0);
+
 }
