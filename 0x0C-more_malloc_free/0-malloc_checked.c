43ce9be52db3115a3e51d08e763050a728f9e2bc
Theophilousjoy <theophilousjoy@gmail.com>
2023-09-07 20:11:51 -0700

    Not Funny

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *all
+	void *ptr
 
-	all = malloc(b);
-	if (all == NULL)
+	ptr = malloc(b);
+	if (ptr == NULL)
 		exit(98);
-	return (all);
+	return (ptr);
 }
