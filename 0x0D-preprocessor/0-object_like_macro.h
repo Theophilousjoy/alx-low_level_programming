95f5c36cff69597032c88759b0f75bd3324bd348
Theophilousjoy <theophilousjoy@gmail.com>
2023-09-11 14:08:00 -0700

    Not funny

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+* File: 0-object_like_macro.h
+*
+* Desc: The Header file
+*/
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef PI_H
+#define PI_H
+/**
+* main:macro PI
+*
+* Desc: value for PI
+*/
+#define PI 3.14159265359
+
+#endif
