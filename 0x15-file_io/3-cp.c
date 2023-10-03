92d5bbd37fba42a03ff0c63638455bf9abd7c471
Theophilousjoy <theophilousjoy@gmail.com>
2023-10-02 19:00:32 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 		}
 		y = write(to, buffer, x);
 		if (to == -1 || y == -1)
-		{
+	{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
